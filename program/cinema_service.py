1354dcebd96d23ddba5e212b95955dab124c4ba9
Mickelll22 <mickelgerges1@gmail.com>
2025-03-02 15:36:27 +0000

    Minimal Progress
    Didnt do anything yet just connected to the table database, will use SQLAlchemy library to manipulate database through python.

--- program/cinema_service.py
+++ program/cinema_service.py
@@ -1,3 +1,15 @@
 # The purpose of this script is to Complete the "CinemaService" class from the Class Diagram created during the design phase.
 
 # Importing libraries:
+import mysql.connector # To connect with the MYsql table created in MYSQL workbench.
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="MickelUWE",
+    password="g<bI1Z11iC]c", 
+    database="Cinema"
+)
+
+# Defining Cinema Class:
+
+#class Cinema:
